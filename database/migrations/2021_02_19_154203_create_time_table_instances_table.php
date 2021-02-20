40d8229b091f6f32b645db8430ec5a39327c6a17
bhargav1240 <bhavu92@gmail.com>
2021-02-20 16:36:43 +0530

    date tested

---
+++ database/migrations/2021_02_19_154203_create_time_table_instances_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimeTableInstancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_table_instances', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_table_instances');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TimeTableInstanceController;
+use App\Models\TimeTableInstance;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $date = now();
+    $str_time = strtotime("15-02-2021");
+    $temp = TimeTableInstance::whereDate('created_at', '<', date('Y-m-d', $str_time))->orderBy('date')->get();
+    dd($date, $str_time, $temp);
 });
