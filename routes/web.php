f001ba0f75973d0da410c59aa7195f5ba7293a47
bhargav1240 <41502470+bhargav1240@users.noreply.github.com>
2021-02-24 00:49:36 +0530

    filter date and time using collection done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TimeTableInstanceController;
 use App\Models\TimeTableInstance;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,36 +21,29 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-    //2010-08-04
-    $start_date = "01-01-2000";
-    $end_date = "31-12-2010";
-    $start_time = "13:00";
-    $end_time = "14:02";
-//    if($start_date != null && $end_date != null){
-//        $temp = TimeTableInstance::whereBetween('date', [
-//            date('Y-m-d', strtotime($start_date)),
-//            date('Y-m-d', strtotime($end_date))
-//        ])
-//            ->orderBy('date')
-//            ->get();
-//    }
-    $temp = TimeTableInstance::
-    whereBetween('date', [
-        date("Y-m-d", strtotime($start_date)),
-        date("Y-m-d", strtotime($end_date))
-    ])
-    ->whereBetween('start_time', [
-        date("H:i", strtotime($start_time)),
-        date("H:i", strtotime($end_time))
-    ])
-        ->whereBetween('end_time', [
-            date("H:i", strtotime($start_time)),
-            date("H:i", strtotime($end_time))
-        ])
-        ->orderBy('date')
-        ->get();
+
+    $start_date = "23-02-2021";
+    $end_date = "23-02-2021";
+    $start_time = "04:00";
+    $end_time = "19:20";
+
+    $full_start_date_time = $start_date .' ' .$start_time;
+    $full_end_date_time = $end_date .' ' .$end_time;
+
+    $collection = TimeTableInstance::all();
+
+    $filtered = $collection->filter(function ($value, $key) use ($full_start_date_time, $full_end_date_time) {
+
+        $value->date_time_start = $value->date . ' ' . $value->start_time;
+        $value->date_time_end = $value->date . ' ' . $value->end_time;
+
+        return $value->date_time_start >= date("Y-m-d H:i", strtotime($full_start_date_time)) && $value->date_time_end <= date("Y-m-d H:i", strtotime($full_end_date_time));
+    });
+
+    $temp = $filtered->all();
+
     return response()->json([
-        'count' => $temp->count(),
+        'count' => count($temp),
         'data' => $temp,
     ]);
 });
